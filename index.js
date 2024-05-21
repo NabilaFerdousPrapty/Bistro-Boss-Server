db6c135c2db284f6f2c7fe51cf1c46d86dd18bde
NabilaFerdousPrapty <128033656+NabilaFerdousPrapty@users.noreply.github.com>
2024-05-22 02:19:31 +0600

    mongo implemented

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const mongodb = require('mongodb');
+ require('dotenv').config();
 const port=process.env.PORT || 5000;
 const app = express();
 
@@ -9,9 +11,49 @@ app.use(express.json());
 app.use(cors());
 
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pflyccd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+const BistroDB=client.db("Bistro-Boss");
+const menuCollection=BistroDB.collection("menu");
+const reviewsCollection=BistroDB.collection("reviews");
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    app.get('/menu', async (req, res) => {
+        const menu = await menuCollection.find({}).toArray();
+        res.send(menu);
+    });
+    app.get('/reviews', async (req, res) => {
+        const reviews = await reviewsCollection.find({}).toArray();
+        res.send(reviews);
+    });
+    
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
 app.get('/', (req, res) => {
     res.send('Bistro Boss Server is running');
 }   );
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
+
+
